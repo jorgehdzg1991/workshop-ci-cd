e766e435bb81f88c00672d0a0d5279e6f06e01e2
Jorge Hernandez <jorgehdzg1991@gmail.com>
2020-08-15 11:24:13 -0500

    add greet endpoint

--- src/controllers/HelloController.js
+++ src/controllers/HelloController.js
@@ -10,9 +10,15 @@ export default class HelloController extends BaseController {
 
   initialize() {
     this.app.get(HelloController.basePath, this.sayHello.bind(this));
+    this.app.get(`${HelloController.basePath}/greet/:name`, this.greet.bind(this));
   }
 
   sayHello(req, res) {
     this.respond(res, OK, { hello: 'world' });
+  }
+
+  greet(req, res) {
+    const { name } = req.params;
+    this.respond(res, OK, { greet: `Hello ${name}!` });
   }
 }
